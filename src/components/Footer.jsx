8d0519a7cf75438104fb1195d601b2f38ad0ed8b
sharminmousumi <sharminmousumi001@gmail.com>
2020-05-12 20:20:13 +0200

    created header footer and background image

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer=()=>{
+
+
+
+
+    return(
+        <div>
+            
+            <div className="footer">
+            <div>
+                Address
+            </div>
+            <div>
+                Phone Number
+            </div>
+            </div>
+            
+
+        </div>
+    )
+}
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Header.css';
+
+const Header=()=>{
+
+
+
+
+    return(
+        <header>
+            <h1>Bobby Car</h1>
+            <div className="item">
+            <div className="shop">
+                Product-List
+            </div>
+            <div>
+                Shopping-Cart
+            </div>
+            </div>
+
+        </header>
+    )
+}
+export default Header;
