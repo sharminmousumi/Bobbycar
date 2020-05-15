7138a7e9047f26b82f2cd5b9ae4b25f36f828bce
sharminmousumi <sharminmousumi001@gmail.com>
2020-05-15 02:37:07 +0200

    create search bar and finished with coding

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './product.css';
 import img1 from '../images/447265.jpeg';
 
@@ -6,36 +6,55 @@ import img1 from '../images/447265.jpeg';
 
 
 const Product = () => {
+    
     const productList=[
-        {name:'Classic Racer', price:600,img:<img src={img1} alt="img1" /> },
-        {name:'Car Delux', price:400},
-        {name:'Car Ledljus', price:700},
-        {name:'Car Classic Fulda', price:200},
-        {name:'Car Neo', price:800},
-        {name:'Car porse', price:600},
-        {name:'Car Bmw', price:400},
-        {name:'Car Mercdeze', price:700},
-        {name:'Car Peoso', price:200},
-        {name:'Car Renult', price:800},
-        {name:'Car Honda', price:200},
-        {name:'Car Toyota', price:800},
+        {name:'Classic Racer', price:600 },
+        {name:'Delux ', price:400},
+        {name:'Ledljus', price:700},
+        {name:'Fulda classic', price:200},
+        {name:'Neo', price:800},
+        {name:'porse', price:600},
+        {name:'Bmw', price:400},
+        {name:'Mercdeze', price:700},
+        {name:'Peoso', price:200},
+        {name:'Renult', price:800},
+        {name:'Honda', price:200},
+        {name:'Toyota', price:800},
         ]
         
         
-        const data=productList.map(item=> (
-            
-            <div  key={item.name}> 
+        const [filter, setFilter]=useState("");
+        const data = productList.map((item)=> {
+            if(filter.length !== 0) {
+                if (item.name.toUpperCase().match(filter.toUpperCase())
+                
+                ) {
+             return <div  key={item.name}> 
+                    <div  className="grid-item">
+                   {item.name}<br/>
+                   {item.price} <br/>
+                    <button className="but">Add to Shopping</button>
+            </div>
+            </div>
+                }
+                else {
+                    return null
+                }
+            }
+          
+    return  <div  key={item.name}> 
             <div  className="grid-item">
-            {item.name}<br/>
-            {item.price} <br/>
+            Model:{item.name}<br/>
+            Price:{item.price} <br/>
             <button className="but">Add to Shopping</button>
             
             </div>
              </div>
             
-        ))
+        })
     return (
         <div>
+            <input className="SearchProduct" type="text" placeholder="search..." value={filter} onChange={(event) => setFilter(event.target.value)} />
             <div className="products">
                 
             {data}
