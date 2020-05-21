e1ce236e5eed14922677b5161a37efc1dfd42787
sharminmousumi <sharminmousumi001@gmail.com>
2020-05-21 03:48:28 +0200

    add the pic

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './product.css';
+import images from '../images/background.jpg';
 
 
 
@@ -8,18 +9,15 @@ import './product.css';
 const Product = () => {
     
     const productList=[
-        {name:'Classic Racer', price:600 },
-        {name:'Delux ', price:400},
-        {name:'Ledljus', price:700},
-        {name:'Fulda classic', price:200},
-        {name:'Neo', price:800},
-        {name:'porse', price:600},
-        {name:'Bmw', price:400},
-        {name:'Mercdeze', price:700},
-        {name:'Peoso', price:200},
-        {name:'Renult', price:800},
-        {name:'Honda', price:200},
-        {name:'Toyota', price:800},
+        {name:'Classic Racer', price:600,im:<img className="set bobby" src="img/bobby1.jpeg" alt="bobby" />},
+        {name:'Delux ', price:400,im:<img className="set bobby" src="img/bobby2.jpg" alt="bobby2" />},
+        {name:'Ledljus', price:700,im:<img className="set bobby" src="img/bobby3.jpg" alt="bobby3" />},
+        {name:'Fulda classic', price:200,im:<img className="set bobby" src="img/bobby4.jpg" alt="bobby" />},
+        {name:'Neo', price:800,im:<img className="set bobby" src="img/bobby5.jpg" alt="bobby" />},
+        {name:'porse', price:600,im:<img className="set bobby" src="img/bobby6.jpg" alt="bobby" />},
+        {name:'Bmw', price:400,im:<img className="set bobby" src="img/bobby7.jpg" alt="bobby" />},
+        {name:'Mercdeze', price:700,im:<img className="set bobby" src="img/bobby8.jpg" alt="bobby" />},
+      
         ]
         
         
@@ -27,13 +25,17 @@ const Product = () => {
         const data = productList.map((item)=> {
             if(sort.length !== 0) {
                 if (item.name.toUpperCase().match(sort.toUpperCase())
+                ||(item.price.toString().match(sort.toString()))
                 
                 ) {
              return <div  key={item.name}> 
                     <div  className="grid-item">
                    {item.name}<br/>
                    {item.price} <br/>
-                    <button className="but">Add to Shopping</button>
+                   <div className="image">
+                    {item.im} 
+                    </div>
+                   
             </div>
             </div>
                 }
@@ -46,19 +48,36 @@ const Product = () => {
             <div  className="grid-item">
             Model:{item.name}<br/>
             Price:{item.price} <br/>
-            <button className="but" >Add to Shopping</button>
+            <div className="image">
+            {item.im} 
+            </div>
+           
             
             </div>
              </div>
             
         })
     return (
-        <div className="search">
-            <input className="SearchProduct" type="text" placeholder="search..." value={sort} onChange={(event) => setSort(event.target.value)} />
-            <div className="products">
-                
-            {data}
+        <div>
+        <div className="container1">
+
+            <img className="backgroundimg"   src={images} alt="background"/>
+            <div className="bottom-right">
+                <h1>Köp din bobby car online</h1>
+                <p>Fri frakt på allt</p>
+                <p>Livstid service</p>
+                <p>Snabbast och säkrast</p>
+            </div>
+            </div>
             
+                <div className="container">
+                    <div className="search">
+                        <p>köp din bil lalala</p>
+                    <input className="SearchProduct" type="text" placeholder="search..." value={sort} onChange={(event) => setSort(event.target.value)} />
+                    </div>
+                    <div className="products"> 
+                    {data}
+                    </div>
             </div>
             
             
