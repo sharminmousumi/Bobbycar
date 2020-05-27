a411efae12bfd6c04d8a7de21e3b686e365a3c9d
sharminmousumi <sharminmousumi001@gmail.com>
2020-05-28 01:40:16 +0200

    Updates

---
+++ precache-manifest.7464ba3874b2165737aff82c90bc8da0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1805d6e3108b820a144768c1e23147d",
+    "url": "/Bobbycar/index.html"
+  },
+  {
+    "revision": "9c1c76a0ecaca7e807a3",
+    "url": "/Bobbycar/static/css/main.2826d31e.chunk.css"
+  },
+  {
+    "revision": "4700b7cbab45be2ce273",
+    "url": "/Bobbycar/static/js/2.f68b744c.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/Bobbycar/static/js/2.f68b744c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c1c76a0ecaca7e807a3",
+    "url": "/Bobbycar/static/js/main.a64d23ae.chunk.js"
+  },
+  {
+    "revision": "882f0c00eeb1e842dc15",
+    "url": "/Bobbycar/static/js/runtime-main.3fa4415e.js"
+  },
+  {
+    "revision": "7de3f599c393a74bd573793dabcad5ef",
+    "url": "/Bobbycar/static/media/background.7de3f599.jpg"
+  },
+  {
+    "revision": "c3f839154414c2f9e2bbd8ad115bca6b",
+    "url": "/Bobbycar/static/media/fb.c3f83915.png"
+  }
+]);
